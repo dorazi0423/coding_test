5a7ed03df8f58805ccf7c3c70e4cf4e71542fc3e
dorazi0423 <dorazi0423@gmail.com>
2025-02-05 12:46:44 +0900

    2025/02/05
    https://www.acmicpc.net/problem/27323

---
+++ 27323번: 직사각형.js
@@ -0,0 +1,16 @@
+const readline=require('readline');
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout
+});
+
+let arr = [];
+
+rl.on('line',(line)=>{
+    arr.push(Number(line));
+
+    if(arr.length===2){
+        console.log(arr[0]*arr[1]);
+        rl.close();
+    }
+});
