c8777b30b65acd0676c58d28b01d90b73c600cab
dorazi0423 <dorazi0423@gmail.com>
2025-02-06 18:14:32 +0900

    2025/02/06
    https://www.acmicpc.net/problem/2798

---
+++ 2798번: 블랙잭.js
@@ -0,0 +1,26 @@
+const readline=require('readline');
+const rl = readline.createInterface({
+    input:process.stdin,
+    ouput:process.stdout
+});
+
+rl.question('',(input)=>{
+    let [N, M] = input.split(' ').map(Number);
+    
+    rl.on('line',(line)=>{
+        let arr = line.split(' ').map(Number);
+        let sum = [];
+        
+        for(let i=0; i<arr.length-2; i++){
+            for(let j=i+1; j<arr.length-1; j++){
+                for(let k=j+1; k<arr.length; k++){
+                    let num = arr[i]+arr[j]+arr[k];
+                    
+                    if(num<=M) sum.push(num);
+                }
+            }
+        }
+        console.log(Math.max(...sum));
+        rl.close();
+    });
+})
