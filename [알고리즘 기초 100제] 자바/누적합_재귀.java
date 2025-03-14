d959857fc96c9412e881462bdf76b2ea0deb4167
dorazi0423 <dorazi0423@gmail.com>
2025-03-14 23:55:25 +0900

    2025/03/14
    https://www.youtube.com/watch?v=hmc_m_eTek4&list=PLVoihNyHW4xkm_KJ8_N8X7F6EQP4uSRyR&index=33

--- [알고리즘 기초 100제] 자바/누적합_재귀.java
+++ [알고리즘 기초 100제] 자바/누적합_재귀.java
@@ -1,1 +1,18 @@
+//https://www.youtube.com/watch?v=hmc_m_eTek4&list=PLVoihNyHW4xkm_KJ8_N8X7F6EQP4uSRyR&index=33
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static int RecursiveFunction(int num){
+        if(num == 1)
+            return 1;
+        else
+            return num + RecursiveFunction(num-1);
+        }
+    public static void main(String[] args) {
+        System.out.println(Main.RecursiveFunction(4));
+    }
+}
 
