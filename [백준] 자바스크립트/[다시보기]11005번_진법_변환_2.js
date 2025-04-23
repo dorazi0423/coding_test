d556580c3796b61dea384ef8a6c97bad06a9c2f8
dorazi0423 <dorazi0423@gmail.com>
2025-04-23 21:27:57 +0900

    2025/04/22
    https://www.acmicpc.net/problem/11005

--- [백준] 자바스크립트/[다시보기]11005번_진법_변환_2.js
+++ [백준] 자바스크립트/[다시보기]11005번_진법_변환_2.js
@@ -1,3 +1,3 @@
-//https://www.acmicpc.net/problem/11005
+//https://www.acmicpc.net/problem/11005 
 const [N, B] = require("fs").readFileSync("/dev/stdin").toString().trim().split(" ");
 console.log(Number(N).toString(Number(B)).toUpperCase());
