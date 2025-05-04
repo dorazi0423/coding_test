979b7df82dde446ebcbfea747d5866f1495ea209
dorazi0423 <dorazi0423@gmail.com>
2025-05-04 15:17:59 +0900

    2025/05/02
    https://www.acmicpc.net/problem/33515

--- [백준] 자바스크립트/33515번_노트북_세_대를_가지고_왔다.js
+++ [백준] 자바스크립트/33515번_노트북_세_대를_가지고_왔다.js
@@ -1,4 +1,4 @@
-//https://www.acmicpc.net/problem/33515
+//https://www.acmicpc.net/problem/33515 
 
 const input = require("fs").readFileSync("/dev/stdin").toString().split(" ").map(Number);
 console.log(Math.min(...input));
